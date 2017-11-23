8aa9f7755d9d20b1f88822cc23fb923178aa05da
skyisbule <heqiut@qq.com>
2017-11-23 23:06:11 +0800

    完成口令缓存

--- src/main/java/com/skyisbule/sso/DataCenter/Session.java
+++ src/main/java/com/skyisbule/sso/DataCenter/Session.java
@@ -1,9 +1,24 @@
 package com.skyisbule.sso.DataCenter;
 
+import com.skyisbule.sso.Model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by skyisbule on 2017/11/23.
  * 此类用于用户Session缓存、同步
  */
 public class Session {
+    public static Map<String,User> SessionMap= new HashMap<String,User>(16);
+
+    public static void put(String session,User user){
+        //如果已存在就清掉更新
+        if (SessionMap.containsKey(session)){
+            SessionMap.remove(session);
+        }
+        //把映射关系丢进map
+        SessionMap.put(session,user);
+    }
 
 }
--- src/main/java/com/skyisbule/sso/DataCenter/Ticket.java
+++ src/main/java/com/skyisbule/sso/DataCenter/Ticket.java
@@ -1,8 +1,18 @@
 package com.skyisbule.sso.DataCenter;
 
+import com.skyisbule.sso.Model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by skyisbule on 2017/11/23.
  * 此类用于用户Ticket缓存、同步
  */
 public class Ticket {
+    public static Map<String,User> TicketMap= new HashMap<String, User>(16);
+
+    public static void put(String Ticket,User user){
+        TicketMap.put(Ticket,user);
+    }
 }
